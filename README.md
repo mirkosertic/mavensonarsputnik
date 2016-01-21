befc493845a9c9b2cb2d1dafd04ae1c047fa804d
sertic <sertic@thalia.de>
2016-01-21 17:56:56 +0100

    First version

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# maven-sputnik
+# Maven Sonar Sputnik integration
 
-Maven Plugin for Sputnik with Multi-Module Support
+Maven Plugin for Sputnik with Multi-Module Support.
+
+Together with Sputnik, Jenkins and Gerrit you can easily setup a pretested commit infrastructure for semi-automatic Code Reviews.
+
+## Usage
+
+The following command can be used in conjunction with the Gerrit Trigger on Jenkins:
+
+```
+mvn de.mirkosertic.mavensonarsputnik:sputnik:sputnik 
+   -DgerritRevision=<GERRIT_REVISION_ID> 
+   -DgerritChangeId=<GERRIT_CHANGE_ID> 
+   -DsputnikConfiguration=<path-to-sputnik.properties> 
+   -DsonarConfiguration=<path-to-sonar.properties
+```
+
+The sputnik.properties file contains authentication information to connect to Gerrit:
+
+```
+connector.host=<Gerrit host>
+connector.path=<Gerrit context>
+connector.port=<Gerrit port>
+connector.username=<Gerrit username>
+connector.password=<Gerrit password>
+```
+
+The sonar.properties file contains authentication information to connect to SonarQube:
+
+```
+sonar.jdbc.url=<JDBC url to SonarQube database>
+sonar.jdbc.driverClassName=<JDBC Driver>
+sonar.jdbc.username=<Sonar username>
+sonar.jdbc.password=<Sonar password>
+sonar.host.url=<URL to Sonar Web UI>
+```
+
---
+++ src/main/java/de/mirkosertic/mavensonarsputnik/MavenSonarSputnikMojo.java
@@ -0,0 +1,181 @@
+package de.mirkosertic.mavensonarsputnik;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import pl.touk.sputnik.configuration.CliOption;
+import pl.touk.sputnik.configuration.Configuration;
+import pl.touk.sputnik.configuration.ConfigurationBuilder;
+import pl.touk.sputnik.configuration.GeneralOption;
+import pl.touk.sputnik.connector.ConnectorFacade;
+import pl.touk.sputnik.connector.ConnectorFacadeFactory;
+import pl.touk.sputnik.connector.ConnectorType;
+import pl.touk.sputnik.engine.Engine;
+
+/**
+ * Execute Sputnik on the project and report the issues to Gerrit.
+ */
+@Mojo(name = "sputnik", defaultPhase = LifecyclePhase.COMPILE)
+public class MavenSonarSputnikMojo extends AbstractMojo {
+
+    @Component
+    private MavenSession mavenSession;
+
+    /**
+     * The gerrit change id.
+     */
+    @Parameter(defaultValue = "${gerritChangeId}", required = true)
+    private String gerritChangeId;
+
+    /**
+     * The gerrit revision id.
+     */
+    @Parameter(defaultValue = "${gerritRevision}", required = true)
+    private String gerritRevision;
+
+    /**
+     * The Sputnik configuration property file..
+     */
+    @Parameter(defaultValue = "${sputnikConfiguration}", required = true)
+    private File sputnikConfiguration;
+
+    /**
+     * The Sonar configuration property file..
+     */
+    @Parameter(defaultValue = "${sonarConfiguration}", required = true)
+    private File sonarConfiguration;
+
+    private Properties sputnikProperties;
+
+    private Properties sonarProperties;
+
+    private void listModulesFor(String aPrefix, MavenProject aProject, int aRecursionLevel, List<MavenProject> aProjectList)
+            throws IOException {
+        StringBuilder theBuilder = new StringBuilder();
+        for (int i=0;i<aRecursionLevel;i++) {
+            theBuilder.append(" ");
+        }
+
+        List<String> theModules = new ArrayList<String>();
+
+        for (MavenProject theProject : aProjectList) {
+            if (theProject.getParent() == aProject) {
+                listModulesFor(theProject.getArtifactId() + ".sonar" , theProject, aRecursionLevel + 1, aProjectList);
+                theModules.add(theProject.getArtifactId());
+            }
+        }
+
+        if (!theModules.isEmpty()) {
+            StringBuilder theModulesConfig = new StringBuilder();
+            for (String theData : theModules) {
+                if (theModulesConfig.length() > 0) {
+                    theModulesConfig.append(",");
+                }
+                theModulesConfig.append(theData);
+            }
+            sonarProperties.put(aPrefix + ".modules" , theModulesConfig.toString());
+        } else {
+
+            StringBuilder theSourceRoots = new StringBuilder();
+            for (String theSourceRoot : aProject.getCompileSourceRoots()) {
+                File theFile = new File(aProject.getBasedir(), theSourceRoot);
+                if (theFile.exists()) {
+                    if (theSourceRoots.length() > 0) {
+                        theSourceRoots.append(",");
+                    }
+                    theSourceRoots.append(theSourceRoot.substring(aProject.getBasedir().toString().length() + 1));
+                }
+            }
+
+            StringBuilder theTestRoots = new StringBuilder();
+            for (String theSourceRoot : aProject.getTestCompileSourceRoots()) {
+                File theFile = new File(aProject.getBasedir(), theSourceRoot);
+                if (theFile.exists()) {
+                    if (theTestRoots.length() > 0) {
+                        theTestRoots.append(",");
+                    }
+                    theTestRoots.append(theSourceRoot.substring(aProject.getBasedir().toString().length() + 1));
+                }
+            }
+
+            sonarProperties.put(aPrefix + ".sources" , theSourceRoots.toString());
+            sonarProperties.put(aPrefix + ".tests" , theTestRoots.toString());
+        }
+
+        String theBaseDir = aProject.getBasedir().toString();
+        sonarProperties.put(aPrefix + ".projectBaseDir", theBaseDir);
+    }
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        try {
+            MavenProject theProject = mavenSession.getCurrentProject();
+
+            sputnikProperties = new Properties();
+            sputnikProperties.load(getClass().getResourceAsStream("/default-sputnik.properties"));
+
+            try (InputStream theStream = new FileInputStream(sputnikConfiguration)) {
+                sputnikProperties.load(theStream);
+            }
+
+            sputnikProperties.setProperty(CliOption.CHANGE_ID.getKey(), gerritChangeId);
+            sputnikProperties.setProperty(CliOption.REVISION_ID.getKey(), gerritRevision);
+
+            sonarProperties = new Properties();
+            sonarProperties.load(getClass().getResourceAsStream("/default-sonar.properties"));
+            sonarProperties.put("sonar.projectName", theProject.getName());
+            sonarProperties.put("sonar.projectVersion", theProject.getVersion());
+            sonarProperties.put("sonar.projectKey", theProject.getGroupId() + ":" + theProject.getArtifactId());
+
+            try (InputStream theStream = new FileInputStream(sonarConfiguration)) {
+                sonarProperties.load(theStream);
+            }
+
+            if (mavenSession.getTopLevelProject() == theProject) {
+                // Run only once at the end of maven execution
+                List<MavenProject> theProjects = mavenSession.getProjects();
+                listModulesFor("sonar", mavenSession.getTopLevelProject(), 0, theProjects);
+
+                File theTempFile = File.createTempFile("sputnik-sonar", ".properties");
+                try (FileOutputStream theStream = new FileOutputStream(theTempFile)) {
+                    sonarProperties.store(theStream, "");;
+                }
+                sputnikProperties.setProperty(GeneralOption.SONAR_PROPERTIES.getKey(), theTempFile.toString());
+
+                Configuration theConfiguration = ConfigurationBuilder.initFromProperties(sputnikProperties);
+
+                ConnectorFacade facade = getConnectorFacade(theConfiguration);
+                new Engine(facade, theConfiguration).run();
+            }
+
+        } catch (Exception e) {
+            throw new MojoExecutionException("Error invoking sputnik", e);
+        }
+    }
+
+    private static ConnectorFacade getConnectorFacade(Configuration configuration) {
+        ConnectorType theConnectorType = ConnectorType
+                .getValidConnectorType(configuration.getProperty(GeneralOption.CONNECTOR_TYPE));
+        ConnectorFacade theFacade = ConnectorFacadeFactory.INSTANCE.build(theConnectorType, configuration);
+        theFacade.validate(configuration);
+        return theFacade;
+    }
+}
